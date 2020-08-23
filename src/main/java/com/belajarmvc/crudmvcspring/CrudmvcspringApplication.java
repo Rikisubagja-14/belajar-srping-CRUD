87fb5da122b829269e7df335ab8b0c5e2a03b73f
unknown <riki43733@gmail.com>
2020-08-23 20:12:39 +0700

    belajar spring CRUD

---
+++ src/main/java/com/belajarmvc/crudmvcspring/CrudmvcspringApplication.java
@@ -0,0 +1,13 @@
+package com.belajarmvc.crudmvcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudmvcspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudmvcspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belajarmvc/crudmvcspring/CrudmvcspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajarmvc.crudmvcspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudmvcspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
